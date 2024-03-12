675e3f3c4d152ac9646684c5314680815668ea78
hadugan700 <patrickstefandarie@gmail.com>
2024-03-12 17:33:46 +0100

    can add tasks from form

--- script.js
+++ script.js
@@ -6,52 +6,6 @@ menuBtn.onclick = function() {
     menuModal.style.display = "block";
 }
 
-fetch('tasks.json')
-    .then(response => response.json())
-    .then(data => {
-        const tasksContainer = document.getElementById('tasks-container');
-        
-        data.tasks.forEach(task => {
-            const taskContainer = document.createElement('div');
-            taskContainer.classList.add('task-container');
-
-            const taskText = document.createElement('div');
-            taskText.classList.add('task-text');
-            taskText.textContent = task.name;
-
-            const taskTime = document.createElement('div');
-            taskTime.classList.add('task-time');
-            taskTime.textContent = task.time;
-
-            const XP = document.createElement('div');
-            XP.classList.add('XP');
-            if (task.XP) {
-                XP.textContent = task.XP;
-            }
-
-            const progBar = document.createElement('div');
-            progBar.classList.add('prog-bar');
-            
-            const img = document.createElement('img');
-            img.src = './images/bar.png';
-            img.alt = 'prog-bar';
-
-            progBar.appendChild(img);
-
-            taskContainer.appendChild(taskText);
-            taskContainer.appendChild(taskTime);
-            
-            if (task.XP) {
-                taskContainer.appendChild(XP);
-            }
-
-            taskContainer.appendChild(progBar);
-
-            tasksContainer.appendChild(taskContainer);
-        });
-    })
-    .catch(error => console.error('Error fetching tasks:', error)); 
-
 let taskModal = document.getElementById("task-modal");
 let taskBtn = document.getElementById("add-btn");
 
@@ -67,3 +21,31 @@ document.onclick = function(event) {
         taskModal.style.display = "none";
     }
 } 
+
+let createBtn = document.getElementById("create-btn");
+let form = document.querySelector('form');
+
+createBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    let formData = new FormData(form);
+    let data = Object.fromEntries(formData);
+    // let jsonData = JSON.stringify(data);
+    // console.log(jsonData);
+
+    // window.localStorage.setItem('task', jsonData);
+    // console.log(window.localStorage.getItem('task'));
+
+    let task = document.createElement("div");
+    task.classList.add("task-container");
+    task.innerHTML = `
+        <div class="task-text">${data['task-name']}</div>
+        <div class="task-time">${data['start-time']} - ${data['end-time']}</div>
+        <div class="prog-bar">
+            <img src="../images/bar-empty.png" alt="prog-bar">
+        </div>`;
+
+    let container = document.getElementById("tasks-container");
+    container.innerHTML += task.outerHTML;
+
+    taskModal.style.display = "none";
+})
