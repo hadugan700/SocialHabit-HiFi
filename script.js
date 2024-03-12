1af6dd590323ff2002d96086f0e851dd6635e2b5
hadugan700 <patrickstefandarie@gmail.com>
2024-03-12 17:25:12 +0100

    can add tasks from form

--- script.js
+++ script.js
@@ -21,3 +21,31 @@ document.onclick = function(event) {
         taskModal.style.display = "none";
     }
 } 
+
+let createBtn = document.getElementById("create-btn");
+let form = document.querySelector('form');
+
+createBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    let formData = new FormData(form);
+    let data = Object.fromEntries(formData);
+    // let jsonData = JSON.stringify(data);
+    // console.log(jsonData);
+
+    // window.localStorage.setItem('task', jsonData);
+    // console.log(window.localStorage.getItem('task'));
+
+    let task = document.createElement("div");
+    task.classList.add("task-container");
+    task.innerHTML = `
+        <div class="task-text">${data['task-name']}</div>
+        <div class="task-time">${data['start-time']} - ${data['end-time']}</div>
+        <div class="prog-bar">
+            <img src="../images/bar-empty.png" alt="prog-bar">
+        </div>`;
+
+    let container = document.getElementById("tasks-container");
+    container.innerHTML += task.outerHTML;
+
+    taskModal.style.display = "none";
+})
