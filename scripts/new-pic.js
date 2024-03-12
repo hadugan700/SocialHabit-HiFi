167b0ec5e40b7458e019559b301d014262258fd2
hadugan700 <patrickstefandarie@gmail.com>
2024-03-12 21:11:19 +0100

    can change profile pic

---
+++ scripts/new-pic.js
@@ -0,0 +1,28 @@
+let picModal = document.getElementById("pic-modal");
+let friendBtn = document.getElementById("new-profile-pic-btn");
+
+friendBtn.onclick = function() {
+    picModal.style.display = "block";
+}
+
+document.onclick = function(event) {
+    if (event.target == picModal) {
+        picModal.style.display = "none";
+    }
+    if (event.target == menuModal) {
+        menuModal.style.display = "none";
+    }
+} 
+
+let createBtn = document.getElementById("pic-save-btn");
+let form = document.getElementById('pic-form');
+
+createBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    let inputPic = document.getElementById("pic-input").files[0]
+
+    let pic = document.getElementById("profile-img")
+    pic.src = `../images/${inputPic.name}`
+
+    picModal.style.display = "none";
+})
