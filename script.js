61d99df7da20bb4c9cad7ca84c121dff395ea66a
FrankLawrence <frankl.am.htg@icloud.com>
2024-03-19 03:14:40 +0100

    Record mouse position and save to JSON

--- script.js
+++ script.js
@@ -69,3 +69,32 @@ document.onclick = function(event) {
         taskModal.style.display = "none";
     }
 } 
+
+// Record mouse position and save to JSON
+// Export JSON with downloadJson()
+let mouseData = [];
+
+function recordMousePos(event) {
+  let currentTime = new Date().toLocaleString();
+  let data = {
+    x: event.clientX,
+    y: event.clientY,
+    time: currentTime
+  };
+  mouseData.push(data);
+}
+
+function downloadJson() {
+  let jsonData = JSON.stringify(mouseData);
+  let blob = new Blob([jsonData], { type: 'application/json' });
+  let url = URL.createObjectURL(blob);
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.download = 'mouse_data.json';
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
+
+document.addEventListener("click", recordMousePos);
